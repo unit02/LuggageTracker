435e1ac85fdda20bc1aa2da5b7bb4a86bb8a67e3
monkeyman707 <Monkey_man_707@live.com>
2016-10-04 17:57:17 +1300

    Added tabbed fragments in sensor view

---
+++ app/src/main/java/mecs/hci/luggagetracker/ViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package mecs.hci.luggagetracker;
+
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentStatePagerAdapter;
+
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new Tab1Fragment();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab1Fragment();
+        }
+        return new Tab1Fragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;           // As there are only 3 Tabs
+    }
+
+}
